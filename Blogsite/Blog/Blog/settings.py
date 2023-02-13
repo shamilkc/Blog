ccf6fdafd94cbce53053ef24d1c905f2df7fea8f
shamil <muhammedshamil299@gmail.com>
2023-02-13 12:20:43 +0530

    database

--- Blogsite/Blog/Blog/settings.py
+++ Blogsite/Blog/Blog/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database',
+        'HOST':'database.cojfqbzuobsn.ap-northeast-1.rds.amazonaws.com',
+        'USER':'admin',
+        'PASSWORD':'munna##**123',
+        'PORT':'3306',
+
     }
 }
 
