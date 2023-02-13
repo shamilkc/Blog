c997fcee7b0de20489413161a9df07b541f6dad6
shamil <muhammedshamil299@gmail.com>
2023-02-13 15:42:03 +0530

    error

--- Blogsite/Blog/Blog/settings.py
+++ Blogsite/Blog/Blog/settings.py
@@ -73,24 +73,24 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database',
-        'HOST':'database.cojfqbzuobsn.ap-northeast-1.rds.amazonaws.com',
-        'USER':'admin',
-        'PASSWORD':'munna##**123',
-        'PORT':'3306',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'database',
+#         'HOST':'database.cojfqbzuobsn.ap-northeast-1.rds.amazonaws.com',
+#         'USER':'admin',
+#         'PASSWORD':'munna##**123',
+#         'PORT':'3306',
+
+#     }
+# }
 
 
 # Password validation
@@ -127,8 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     
